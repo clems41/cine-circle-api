fdb201647af79d766c50e118e2473c0dd423d6a1
Clement NIOT <cniot@isi.nc>
2021-04-26 17:27:15 +1100

    feat(tests): using clean and new database before running testcase

--- cmd/test-template/main.go
+++ cmd/test-template/main.go
@@ -4,12 +4,7 @@ import (
 	"cine-circle/internal/logger"
 	"cine-circle/internal/repository"
 	"cine-circle/internal/test"
-	"cine-circle/internal/utils"
 	"fmt"
-)
-
-var (
-	testDatabaseName = utils.GetDefaultOrFromEnv(test.DefaultTestDatabaseName, test.EnvTestDatabaseName)
 )
 
 func main() {
@@ -28,7 +23,7 @@ func main() {
 
 	// Remove all connections from old test database
 	err = DB.
-		Exec(fmt.Sprintf("select pg_terminate_backend(pid) from pg_stat_activity where datname='%s';", testDatabaseName)).
+		Exec(fmt.Sprintf("select pg_terminate_backend(pid) from pg_stat_activity where datname='%s';", test.TemplateDatabaseName)).
 		Error
 	if err != nil {
 		logger.Sugar.Fatalf(err.Error())
@@ -36,7 +31,7 @@ func main() {
 
 	// Delete old testing database
 	err = DB.
-		Exec(fmt.Sprintf("DROP DATABASE IF EXISTS %s", testDatabaseName)).
+		Exec(fmt.Sprintf("DROP DATABASE IF EXISTS %s", test.TemplateDatabaseName)).
 		Error
 	if err != nil {
 		logger.Sugar.Fatalf(err.Error())
@@ -44,7 +39,7 @@ func main() {
 
 	// Create new testing database
 	err = DB.
-		Exec(fmt.Sprintf("CREATE DATABASE %s", testDatabaseName)).
+		Exec(fmt.Sprintf("CREATE DATABASE %s", test.TemplateDatabaseName)).
 		Error
 	if err != nil {
 		logger.Sugar.Fatalf(err.Error())
@@ -52,7 +47,7 @@ func main() {
 
 	// Close and open connection on new testing database
 	closeFunction()
-	DB, err = repository.OpenConnection(testDatabaseName)
+	DB, err = repository.OpenConnection(test.TemplateDatabaseName)
 	if err != nil {
 		logger.Sugar.Fatalf(err.Error())
 	}
@@ -67,8 +62,8 @@ func main() {
 	repositories := repository.NewAllRepositories(DB)
 	repositories.Migrate()
 
-/*	// Make sure the template is not modified.
-	err = DB.Exec(fmt.Sprintf("ALTER DATABASE %s WITH ALLOW_CONNECTIONS 0;", testDatabaseName)).Error
+	// Make sure the template is not modified.
+/*	err = DB.Exec(fmt.Sprintf("ALTER DATABASE %s WITH ALLOW_CONNECTIONS 0;", test.TemplateDatabaseName)).Error
 	if err != nil {
 		logger.Sugar.Fatalf(err.Error())
 	}*/
--- internal/test/repository.go
+++ internal/test/repository.go
@@ -1,34 +1,65 @@
 package test
 
 import (
+	"cine-circle/internal/logger"
 	"cine-circle/internal/repository"
-	"cine-circle/internal/utils"
+	"fmt"
 	"gorm.io/gorm"
 	"testing"
 )
 
 const (
-	EnvTestDatabaseName = "TEST_DATABASE_NAME"
-	DefaultTestDatabaseName = "test_cine_circle"
+	TemplateDatabaseName = "test_template_db"
+	TestingDatabaseNamePrefix = "testing_database_"
 )
 
-var (
-	testDatabaseName = utils.GetDefaultOrFromEnv(DefaultTestDatabaseName, EnvTestDatabaseName)
-)
+var DatabaseIndexName = 0
 
 func OpenDatabase(t *testing.T) (DB *gorm.DB, closeFunction func()) {
-	// Open connection
-	DB, err := repository.OpenConnection(testDatabaseName)
+	// Open connection with real database for creating testing database
+	realDB, err := repository.OpenConnection()
 	if err != nil {
 		t.Fatalf(err.Error())
 	}
+
+	testingDatabaseName := fmt.Sprintf("%s%d", TestingDatabaseNamePrefix, DatabaseIndexName)
+	DatabaseIndexName++
+
+	err = realDB.
+		Exec(fmt.Sprintf("CREATE DATABASE %s WITH TEMPLATE %s", testingDatabaseName, TemplateDatabaseName)).
+		Error
+	if err != nil {
+		t.Fatalf(err.Error())
+	}
+
+	// Using new clean database for testing
+	testingDB, err := repository.OpenConnection(testingDatabaseName)
+	if err != nil {
+		t.Fatalf(err.Error())
+	}
+
 	// create closeFunction : to be used in testcases
 	closeFunction = func() {
-		err = repository.CloseConnection(DB)
+		// closing testing database before deleting it from real database
+		err = repository.CloseConnection(testingDB)
+		if err != nil {
+			t.Fatalf(err.Error())
+		}
+
+		// delete testing database
+		err = realDB.
+			Exec(fmt.Sprintf("DROP DATABASE IF EXISTS %s", testingDatabaseName)).
+			Error
+		if err != nil {
+			logger.Sugar.Fatalf(err.Error())
+		}
+
+		// closing connection from real database
+		err = repository.CloseConnection(realDB)
 		if err != nil {
 			t.Fatalf(err.Error())
 		}
 	}
 
-	return
+	return testingDB, closeFunction
 }
