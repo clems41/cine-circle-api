1e39d4e1a1b1570c84e22b41f7016c04321e395b
Clement NIOT <cniot@isi.nc>
2021-06-03 15:17:21 +1100

    chore(ci): add script for deploying api

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -8,8 +8,8 @@ APP_NAME=cine-circle-api
 DOCKER_TAG=${REGISTRY}/${GROUP}/${APP_NAME}:${COMMIT_TAG}
 IMAGE_NAME="${REGISTRY}\/${GROUP}\/${APP_NAME}:${COMMIT_TAG}"
 
-#docker build . -t "${DOCKER_TAG}"
-#docker push "${DOCKER_TAG}"
+docker build . -t "${DOCKER_TAG}"
+docker push "${DOCKER_TAG}"
 kubectl --context=bureau get deploy cine-circle-api -n incubator-isi -o yaml | tee deploy.yaml \
 && sed -i "s/image:.*$/image: ${IMAGE_NAME}/g" deploy.yaml \
 && kubectl replace -f deploy.yaml
